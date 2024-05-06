46347a9828a853eec4a5aa42c21d9219939bd743
alxthm <git@alxthm.com>
2024-05-06 16:14:40 +0200

    feat: add treemap plot export

--- src/visualization/export.py
+++ src/visualization/export.py
@@ -9,6 +9,8 @@ from src.visualization.panel_figures.profiles import (
     plot_type_structure,
     plot_annee_publication,
     plot_mois_publication,
+    secteur_activite_n_entites_treemap,
+    secteur_activite_ratio_treemap,
 )
 
 EXPORT_FOLDER = Path(
@@ -71,6 +73,21 @@ def gen_images_02(df, root):
     )
 
 
+def gen_images_03(df, root):
+    shutil.rmtree(root, ignore_errors=True)
+    root.mkdir(parents=True)
+    i = 0
+    fig = secteur_activite_n_entites_treemap(df).object
+    name = f"{i:2d}-n_entites_treemap"
+    for suffix in ("png", "svg", "pdf"):
+        fig.write_image(root / f"{name}.{suffix}", scale=3)
+    i += 1
+    fig = secteur_activite_ratio_treemap(df).object
+    name = f"{i:2d}-ratio_treemap"
+    for suffix in ("png", "svg", "pdf"):
+        fig.write_image(root / f"{name}.{suffix}", scale=3)
+
+
 def profils_entreprises(df, blog_post):
     root = EXPORT_FOLDER / "01-profils-entreprises/img"
 
@@ -79,6 +96,9 @@ def profils_entreprises(df, blog_post):
 
     if "02" in blog_post:
         gen_images_02(df, root / "02")
+
+    if "03" in blog_post:
+        gen_images_03(df, root / "03")
 
 
 def main(blog_posts):
