5fff89977c809b9ca3021d4475c6faccd56e0d7f
alxthm <git@alxthm.com>
2024-06-21 14:04:56 +0200

    ci: add flake.nix

---
+++ flake.nix
@@ -0,0 +1,42 @@
+# taken from https://github.com/DeterminateSystems/zero-to-nix/blob/main/nix/templates/dev/python/flake.nix
+{
+  description = "Python development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      allSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      # Helper to provide system-specific attributes
+      forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in
+    {
+      # Development environment output
+      devShells = forAllSystems ({ pkgs }: {
+        default =
+          let
+            python = pkgs.python311;
+          in
+          pkgs.mkShell {
+            # The Nix packages provided in the environment
+            packages = [
+              # Python plus helper tools
+              (python.withPackages (ps: with ps; [
+                virtualenv
+                pip
+              ]))
+            ];
+          };
+      });
+    };
+}
